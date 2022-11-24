22fb4158bfa395ec68246ed1f6a1adb0477b4d8d
周全 <quan0zhou@163.com>
2022-11-24 19:42:21 +0800

    update setting
    Update SmartCityContext.cs

---
+++ SmartCityWebApi/Models/Setting.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartCityWebApi.Models
+{
+    public class Setting
+    {
+        public string NotifyUrl { get; set; } = default!;
+        public string LimitWeekName { get; set; } = default!;
+        public string LimitTimeOfDay { get; set; } = default!;
+    }
+}
